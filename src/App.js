62c57611434c9ed1e34b6f1fcbf70481ac080b8d
biyumeng <609814862@qq.com>
2020-03-08 20:08:19 +0800

    配置路由

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Button , Checkbox } from 'antd-mobile'
+// import { Button , Checkbox } from 'antd-mobile'
 
-const CheckboxItem = Checkbox.CheckboxItem;
+import { BrowserRouter as Router, Route , Link , Switch } from 'react-router-dom'
+
+// const CheckboxItem = Checkbox.CheckboxItem;
+//导入路由对应组件
+import Home from './pages/Home';
+import CityList from './pages/CityList';
+import Map from './pages/Map';
+import NotFount from './pages/NotFount';
 
 function App() {
   return (
-    <div className="app">
-      <Button type="primary">ant按钮</Button>
-      <CheckboxItem key="disabled" data-seed="logId" disabled defaultChecked multipleLine>
-          Undergraduate
-      </CheckboxItem>
-    </div>
+    <Router className="app">
+      <Link to="/home">首页</Link>
+      <Link to="/cityList">城市列表</Link>
+      <Link to="/map">地图</Link>
+      <Switch>
+        {/* 一级路由 */}
+        <Route path="/home" component={Home}></Route>
+        <Route path="/cityList" component={CityList}></Route>
+        <Route path="/map" component={Map}></Route>
+
+        {/* 404页面 */}
+        <Route component={NotFount}></Route>
+      </Switch>
+      
+    </Router>
   );
 }
 
---
+++ src/pages/NotFount/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFount = ()=> <center><h1>页面找不到：404</h1></center>;
+
+export default NotFount;
