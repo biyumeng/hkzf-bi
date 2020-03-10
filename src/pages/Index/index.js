7beebbfeb0b2b0a097a9e49e098d81f9db39d824
biyumeng <609814862@qq.com>
2020-03-10 17:37:20 +0800

    axios全局配置和拦截器

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -3,21 +3,32 @@ import React, { Component } from 'react';
 //引入走马灯
 import { Carousel } from 'antd-mobile';
 
+import axios , {BASE_URL} from '../../utils/axios'
+// import { log } from 'util';
+
 class Index extends Component {
     state = {
         //轮播图数据
-        data: ['1', '2', '3'],
+        swiper: [],
         //轮播图高度
         imgHeight: 176,
       }
       componentDidMount() {
-        // simulate img loading
-        setTimeout(() => {
-          this.setState({
-            data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-          });
-        }, 100);
+        this.getSwiper()
       }
+
+      //获取轮播图数据
+      getSwiper = async()=>{
+        const res = await axios.get('/home/swiper')
+        // console.log(res.data.body);
+        const {body,status} = res.data
+        if(status === 200 ){
+            this.setState({
+                swiper:body
+            })
+        }
+      }
+
       render() {
         return (
           <div>
@@ -26,21 +37,21 @@ class Index extends Component {
               autoplay={true}
                 //循环播放
               infinite
-              beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-              afterChange={index => console.log('slide to', index)}
             >
-              {this.state.data.map(val => (
+              {/* 列表渲染 */}
+              {this.state.swiper.map(val => (
                 <a
-                  key={val}
-                  href="http://www.alipay.com"
-                  style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                  key={val.id}
+                  href="http://www.itheima.com"
+                  style={{ display: 'inline-block', width: '100%', background: 'gray' , height: this.state.imgHeight }}
                 >
                   <img
-                    src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                    src={`${BASE_URL}${val.imgSrc}`}
                     alt=""
                     style={{ width: '100%', verticalAlign: 'top' }}
                     onLoad={() => {
                       // fire window resize event to change height
+                      //窗口自适应
                       window.dispatchEvent(new Event('resize'));
                       this.setState({ imgHeight: 'auto' });
                     }}
---
+++ src/utils/axios.js
@@ -0,0 +1,38 @@
+// 全局axios
+
+import axios from 'axios'
+
+//后端接口的基础路径
+const BASE_URL = 'http://api-haoke-dev.itheima.net'
+
+//创建一个全局axios实例
+const axios_my = axios.create({
+    baseURL: BASE_URL
+});
+
+// 注册拦截器（request和response）
+
+// Add a request interceptor
+axios_my.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    // console.log('开始请求了');
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+  
+  // Add a response interceptor
+  axios_my.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    // console.log('请求成功了');
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
+  
+  export { BASE_URL }
+  export default axios_my;
